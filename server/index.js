3fedefe7a4a861f5be73692724e407fae03723e4
HolyCloudDevOps <hollydevops@gmail.com>
2024-12-11 18:25:40 -0500

    Add .dockerignore and git ignore, add nginx and fixed bugs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const keys = require('./keys');
 
+// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,69 +9,76 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-// Postgress client setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort,
-    ssl:
-      process.env.NODE_ENV !== 'production'
-        ? false
-        : { rejectUnauthorized: false },
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+  ssl:
+    process.env.NODE_ENV !== 'production'
+      ? false
+      : { rejectUnauthorized: false },
 });
 
-pgClient.on("connect", (client) => {
-    client
-      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-      .catch((err) => console.error(err));
+pgClient.on('connect', (client) => {
+  client
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.error(err));
 });
 
-//Redis Client Setup
+// Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
 app.get('/', (req, res) => {
-    res.send('Hi');
+  res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * from values');
+  const values = await pgClient.query('SELECT * from values');
 
-    res.send(values.rows);
+  res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send(values);
-    });
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
 app.post('/values', async (req, res) => {
     const index = req.body.index;
-
-    if (parseInt(index) > 40) {
-        return res.status(422).send('Index too high');
+  
+    // Перевіряємо, чи є index числом і чи не більше 40
+    if (!index || isNaN(index) || parseInt(index) > 40) {
+      return res.status(422).send('Invalid index or Index too high');
     }
-
-    redisClient.hset('values', index, 'Nothing yet!');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
+  
+    // Конвертуємо index в ціле число перед вставкою
+    const intIndex = parseInt(index);
+  
+    // Вставка значення у Redis
+    redisClient.hset('values', intIndex, 'Nothing yet!');
+    
+    // Публікація в Redis
+    redisPublisher.publish('insert', intIndex);
+  
+    // Вставка значення у PostgreSQL
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [intIndex]);
+  
     res.send({ working: true });
-});
-
-app.listen(5000, err => {
-    console.log('Listening');
+  });
+  
+app.listen(5000, (err) => {
+  console.log('Listening');
 });
